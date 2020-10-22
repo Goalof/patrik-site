02cd6ad64753203b4bb9df9e1a01002db9c6f85e
Goalof <68434460+Goalof@users.noreply.github.com>
2020-10-22 16:48:22 +0600

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,36 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f82bd438a25ab001e4e7639",
+				"5f82bd438a25ab001e4e763b"
+			]
+		},
+		"5f82bd438a25ab001e4e7639": {
+			"id": "5f82bd438a25ab001e4e7639",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f82bd438a25ab001e4e763b": {
+			"id": "5f82bd438a25ab001e4e763b",
+			"name": "index",
+			"pageUrl": "index",
+			"seo": {
+				"og:title": "Электронная виза в Россию от TravelRussiaToday",
+				"title": "Электронная виза в Россию от TravelRussiaToday",
+				"description": "Электронная виза в Россию",
+				"og:description": "Электронная виза в Россию от TravelRussiaToday",
+				"og:image": "https://static.tildacdn.com/tild3365-3965-4062-b565-333661316334/logo2.svg",
+				"favicon_32px": "https://uploads.quarkly.io/5f82bd438a25ab001e4e7636/images/favicons.png?v=2020-10-20T08:26:54.650Z",
+				"favicon_apple_152px": "https://uploads.quarkly.io/5f82bd438a25ab001e4e7636/images/favicons.png?v=2020-10-20T08:26:54.650Z",
+				"favicon_win10_270px": "https://uploads.quarkly.io/5f82bd438a25ab001e4e7636/images/favicons.png?v=2020-10-20T08:26:54.650Z",
+				"background_win10": "https://uploads.quarkly.io/5f82bd438a25ab001e4e7636/images/favicons.png?v=2020-10-20T08:26:54.650Z"
+			}
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
